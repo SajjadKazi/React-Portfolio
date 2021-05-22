2114e2945edfa3a3c0efa41c89523fd7d0ff09c0
SajjadKazi <sajjadkazi4@gmail.com>
2021-05-22 19:24:06 -0400

    React Router is Setup, created all my pages, added Bootstrap package for styling

---
+++ src/NoMatch.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const NoMatch = ()=> (
+    
+        <div>
+        <h1>Random Page</h1>
+        </div>
+    )
+
+export default NoMatch;
---
+++ src/Projects.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Projects = ()=> (
+    
+        <div>
+        <h1>Projects Page </h1>
+        </div>
+    )
+
+export default Projects;
